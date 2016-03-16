8022e43129757af366585fe7941312c2164bfd06
Kimmy Leo <kenpusney@gmail.com>
2016-03-16 10:51:35 +0800

    extract header file

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-clang++ src/*.cpp -std=c++1z -stdlib=libc++ -dynamiclib -o libropto.dylib
+clang++ src/*.cpp -std=c++1z -stdlib=libc++ -Iinclude -dynamiclib -o libropto.dylib
 
-clang++ test/*.cpp -std=c++1z -stdlib=libc++ -Isrc -L. -lropto -o run_test
+clang++ test/*.cpp -std=c++1z -stdlib=libc++ -Iinclude -L. -lropto -o run_test
 
